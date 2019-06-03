51ddafa7fc96b725eeb3593e67a5507ee9263122
AustinNguyen2909 <50312158+AustinNguyen2909@users.noreply.github.com>
2019-06-03 11:08:47 +0700

    My first upload of my jobTest for a Korean Company

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a project to test my skill for a job position
---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySideNav").style.width = "100%"
+    document.getElementById("sideNavIcon").style.display = "none"
+}
+
+function closeNav() {
+    document.getElementById("mySideNav").style.width = "0"
+    document.getElementById("sideNavIcon").style.display = "flex"
+}
